391ea6e4bdc65c2d7e48b4c3f86b23075eb006b1
GonzaBianchi <gonzalobianchini70@gmail.com>
2025-10-08 19:23:19 -0300

    Estilizado flecha volver sidebar colapsable v6

--- frontend/src/components/guild-settings/Sidebar.jsx
+++ frontend/src/components/guild-settings/Sidebar.jsx
@@ -85,7 +85,7 @@ export function Sidebar({ activeSection, setActiveSection, hasChanges, guilds = 
               className="w-full flex items-center justify-center px-4 py-3 rounded-lg transition-all hover:cursor-pointer"
             >
               <div className='flex items-center'>
-                <ArrowLeft className="w-5 h-5" />
+                <ArrowLeft className="w-5 h-5 text-white" />
               </div>
             </button>
           )}
